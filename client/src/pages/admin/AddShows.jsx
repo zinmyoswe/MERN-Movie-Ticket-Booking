f8be62c765226932767f9ed056109090a635792d
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-13 07:40:27 +0700

    Admin Pages and directory

---
+++ client/src/pages/admin/AddShows.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddShows = () => {
+  return (
+    <div>AddShows</div>
+  )
+}
+
+export default AddShows
---
+++ client/src/pages/admin/ListBookings.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListBookings = () => {
+  return (
+    <div>ListBookings</div>
+  )
+}
+
+export default ListBookings
