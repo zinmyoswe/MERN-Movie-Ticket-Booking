1a9e6c967153266e578df31f907406be68ac367f
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-15 15:47:23 +0700

    Booking Model, checkSeatsAvailability, and creatbooking function

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,62 @@
+import Show from "../models/Show.js"
+
+//Function to check availability of selected for a movie
+const checkSeatsAvailability = async (showId, selectedSeats) => {
+  try{
+    const showData = await Show.findById(showId)
+    if(!showData) return false;
+
+    const occupiedSeats = showData.occupiedSeats;
+
+    const isAnySeatTaken = selectedSeats.some(seat => occupiedSeats[seat]);
+
+    return !isAnySeatTaken;
+  }catch(error){
+    console.log(error.message);
+    return false;
+  }
+}
+
+
+export const createBooking = async (req, res) => {
+  try{
+    const {userId} = req.auth();
+    const {showId, selectedSeats} = req.body;
+    const {origin} = req.headers;
+
+    //check if the seat is available for the selected show
+    const isAvailable = await checkSeatsAvailability(showId, selectedSeats)
+
+    if(!isAvailable){
+        return res.json({success: false, message: "Selected Seats are not available."})
+    }
+
+    //Get the show details
+    const showData = await Show.findById(showId).populate('movie');
+
+    //Create a new booking
+    const booking = await Booking.create({
+        user: userId,
+        show: showId,
+        amount: showData.showPrice * selectedSeats.length,
+        bookSeats: selectedSeats
+    })
+
+    selectedSeats.map((seat)=>{
+        showData.occupiedSeats[seat] = userId;
+    })
+
+    showData.markModified('occupiedSeats');
+
+    await showData.save();
+
+    //Stripe Gateway Initialize
+
+    res.json({success:true, message: 'Booked successfully'})
+
+  }catch(error){
+    console.log(error.message);
+    res.json({success:false, message: error.message})
+  }
+}
+
---
+++ server/models/Booking.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    user: {type: String, required: true, ref: 'Movie'},
+    show: {type: String, required: true, ref: 'Show'},
+    amount: {type: Number, required: true},
+    bookedSeats: {type: Array, required: true},
+    isPaid: {type: Boolean, default: false},
+    paymentLink: {type: String},
+}, {timestamps: true}
+)
+
+const Booking = mongoose.model('Booking', bookingSchema)
+
+export default Booking;
