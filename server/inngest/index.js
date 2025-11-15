c3f8a803ee2c0f7ec152786e35cfc2c0adabbfdf
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-15 09:10:31 +0700

    Implement usercreate, update, delete and vercel json for deployment

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,8 +1,51 @@
 import { Inngest } from "inngest";
+import User from '../models/User';
 
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+//Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const {id, first_name, last_name, email_addresses, image_url} = event.data
+    const userData = {
+        _id: id,
+        email: email_addresses[0].email_address,
+        name: first_name + ' ' + last_name,
+        image: image_url
+    }
+    await User.create(userData)
+  },
+);
+
+//Inngest Function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const {id} = event.data
+    await User.findByIdAndDelete(id)
+  },
+);
+
+// Inngest Function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const {id, first_name, last_name, email_addresses, image_url} = event.data
+    const userData = {
+        _id: id,
+        email: email_addresses[0].email_address,
+        name: first_name + ' ' + last_name,
+        image: image_url
+    }
+    await User.findByIdAndUpdate(id, userData)
+  },
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
