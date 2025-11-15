7b2e3932cdd6d9e1c9da7e51e3e99a8f29b40f34
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-15 09:56:22 +0700

    Update db

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try{
         mongoose.connection.on('connected', () => console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/pcineplex`)
     } catch(error){
         console.log(error.message);
     }
