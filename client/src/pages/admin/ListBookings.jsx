c1e3e6aff2e9b7c4ce5bf93aaf04f668460f7d57
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-16 15:03:56 +0700

    fetch ListBooking data in AdminDashboard

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -3,21 +3,34 @@ import { dummyBookingData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import { dateFormat } from '../../lib/dateFormat';
+import { useAppContext } from '../../context/AppContext';
 
 const ListBookings = () => {
+
+  const {axios, getToken, user} = useAppContext()
+
   const currency = import.meta.env.VITE_CURRENCY
 
   const [bookings, setBookings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const getAllBookings = async () => {
-    setBookings(dummyBookingData)
-    setIsLoading(false);
+    try {
+      const {data} = await axios.get('/api/admin/all-bookings',{
+        headers: { Authorization: `Bearer ${await getToken()}`}
+      });
+      setBookings(data.bookings)
+    } catch (error) {
+      console.error(error)
+    }
+    setIsLoading(false)
   };
 
   useEffect(() => {
-    getAllBookings();
-  }, []);
+    if(user){
+      getAllBookings();
+    }
+  }, [user]);
   return !isLoading ? (
     <>
       <Title text1="List" text2="Booings" />
