e5e4a0585a6e0de9a85dd3bd9c30a281f7ca3613
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-16 09:15:37 +0700

    Implement addShow movies, Datetime, price from frontend to database

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -5,6 +5,7 @@ import Title from '../../components/admin/Title.jsx';
 import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react';
 import { kConverter} from '../../lib/kConverter';
 import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast';
 
 const AddShows = () => {
 
@@ -16,6 +17,7 @@ const AddShows = () => {
   const [dateTimeSelection, setDateTimeSelection] = useState({});
   const [dateTimeInput, setDateTimeInput] = useState("");
   const [showPrice, setShowPrice] = useState("");
+  const [addingShow, setAddingShow] = useState(false)
 
   const fetchNowPlayingMovies = async () => {
     try {
@@ -57,11 +59,46 @@ const AddShows = () => {
     })
   }
 
+  const handleSubmit = async () =>{
+    try {
+      setAddingShow(true)
+
+      if(!selectedMovie || Object.keys(dateTimeSelection).length === 0 || !showPrice){
+        return toast('Missing required fields');
+      }
+      const showsInput = Object.entries(dateTimeSelection).map(([date, time]) =>
+      ({date, time}));
+
+      const payload = {
+        movieId: selectedMovie,
+        showsInput,
+        showPrice: Number(showPrice)
+      }
+      const { data } = await axios.post('/api/show/add', payload, {headers:{
+        Authorization: `Bearer ${await getToken()}`
+      }})
+
+      if(data.success){
+        toast.success(data.message)
+        setSelectedMovie(null)
+        setDateTimeSelection({})
+        setShowPrice("")
+      }
+      else{
+        toast.success(data.message)
+      }
+    } catch (error) {
+      console.error("Submission error : ", error);
+      toast.error('An error occured. Please try again.')
+    }
+    setAddingShow(false)
+  }
+
   useEffect(() => {
     if(user){
       fetchNowPlayingMovies();
     }
-  },[]);
+  },[user]);
 
   return nowPlayingMovies.length > 0 ? (
     <>
@@ -147,7 +184,7 @@ const AddShows = () => {
           </ul>
         </div>
       )}
-      <button className='bg-primary text-white px-8 py-2 mt-6 rounded 
+      <button onClick={handleSubmit} disabled={addingShow} className='bg-primary text-white px-8 py-2 mt-6 rounded 
       hover:bg-primary/90 transition-all cursor-pointer'>Add Show</button>
     </>
   ) : <Loading />
