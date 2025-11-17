e014b8f8f97ed72d5e48f4016695f02e8d3d72ef
ZIN MYO SWE <overmidnight12@gmail.com>
2025-11-17 16:02:34 +0700

    update MyBooking Pay Now link

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -4,6 +4,7 @@ import BlurCircle from '../components/BlurCircle';
 import timeFormat from '../lib/timeFormat';
 import { dateFormat } from '../lib/dateFormat';
 import { useAppContext } from '../context/AppContext';
+import { Link } from 'react-router-dom';
 
 const MyBookings = () => {
   const currency = import.meta.env.VITE_CURRENCY;
@@ -57,8 +58,8 @@ const MyBookings = () => {
           <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
             <div className='flex items-center gap-4'>
               <p className='text-2xl font-semibold mb-3'>{currency} {item.amount}</p>
-              {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3
-              text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
+              {!item.isPaid && <Link to={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3
+              text-sm rounded-full font-medium cursor-pointer'>Pay Now</Link>}
             </div>
             <div className='text-sm'>
               <p><span>Total Tickets:</span> {item.bookedSeats.length}</p>
